3d5806ec30fdf1f4745e09b87be7f2bf3b3b6eda
Gabriel Viso Carrera <gvisoc@gmail.com>
2018-06-07 19:19:57 +1000

    Initial commit

--- hello-service/target/classes/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nohup java -cp /opt/hello:/opt/hello/* com.gvisoc.hello.Service > /tmp/hello.out 2>&1 &
-echo $! > /var/run/hello.pid
--- hello-service/target/classes/stop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/kill -15 `cat /var/run/hello.pid`
