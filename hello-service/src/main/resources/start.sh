af6f59b6bba07a5073ac624fcbe81b80f9530abd
Gabriel Viso Carrera <gvisoc@gmail.com>
2018-07-18 17:02:20 +1000

    Gracefully stopping the service before removal.

--- hello-service/src/main/resources/start.sh
+++ hello-service/src/main/resources/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 nohup java -cp /opt/hello:/opt/hello/* -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false com.gvisoc.hello.Start > /tmp/hello.out 2>&1 &
 
-# This (I think) is not needed any more.
 echo $! > /tmp/hello.pid
